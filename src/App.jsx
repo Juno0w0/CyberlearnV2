87b2aa1b50507e6077a4f0b21015606371244524
Juno0w0 <fgonzalezjrb@gmail.com>
2026-01-14 14:32:29 -0600

    Added /components and /pages dir

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from "./Header";
-import Footer from "./Footer";
-import Home from "./Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 function App() {
   return (
